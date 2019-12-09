3b72de8fa40099208586123fb54948ddcfe4223e
Amit Kumar <amit.kumar.anand11@gmail.com>
2019-12-09 15:47:33 +0530

    First Commit

---
+++ ValidateText/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+
+
+
+# Create your views here.
+
+
+
+def index(request):
+    return render(request,'Home.html')
+
+def analyze(request):
+    if request.method == 'GET':
+        dj_text = request.GET.get('text', 'default')
+        puntuation = '~!#$%@^&*()_+=|\/?,<.>[]{}'
+
+        removepunc = request.GET.get('removepunc','off')
+        uppercase = request.GET.get('uppercase','off')
+        newlineremover = request.GET.get('newlineremover','off')
+        extraspaceremover = request.GET.get('extraspaceremover','off')
+
+        if removepunc == 'on':
+            analyzed = ''
+            for char in dj_text:
+                if char not in puntuation:
+                    analyzed+=char
+            dj_text = analyzed
+
+        if uppercase == 'on':
+            analyzed = ''
+            for char in dj_text:
+                analyzed += char.upper()
+            dj_text = analyzed
+
+        if newlineremover == 'on':
+            analyzed = ''
+            for char in dj_text:
+                if char != '\n' and char != '\r':
+                    analyzed += char
+            dj_text = analyzed
+
+        if extraspaceremover == 'on':
+            analyzed = ''
+            for index, char in enumerate(dj_text):
+                if not(dj_text[index] == ' ' and dj_text[index+1] == ' '):
+                    analyzed += char
+
+        if (removepunc != 'on' and uppercase != 'on' and newlineremover != 'on' and extraspaceremover != 'on'):
+            context = {
+                'Purose': 'No Text Found',
+                'text': analyzed
+            }
+        else:
+            context = {
+                'Purose': 'Analyzed Text',
+                'text': analyzed
+            }
+        return render(request,'analyze.html', context)
